349e152475a4ffde797126161405b48be257b561
aqqmin <n.quinnallen@gmail.com>
2023-05-07 23:54:36 +0000

    welll there is further progress but this is still wrong

--- rl_gamblers.py
+++ rl_gamblers.py
@@ -10,10 +10,10 @@ implement uniform random policy
 import random as rnd
 
 N = 100
-X = 50
+X = 45
 PH = .4
-E = .25
-D = .9
+E = .75
+D = .3
 
 
 class Agent(object):
@@ -30,13 +30,21 @@ class Agent(object):
         for i in range(1,N):
             self.memory[i]={}
         for k in self.memory:
-            for j in range(1,k+1):
+            print(k)
+            needed = N-k
+            highest_bet = 0
+            if needed < k:
+                highest_bet = needed
+            else:
+                highest_bet  = k
+            for j in range(1,highest_bet+1):
                 self.memory[k][j]=self.initValue
         
 
     def choose_action(self):
+        choose_from = list(self.memory[self.state].keys())
         if rnd.random() < self.epsilon:
-            return rnd.randrange(1, self.state + 1)
+            return rnd.choice(choose_from)
         else:
             highest_val = 0
             key_of = 1
@@ -45,13 +53,13 @@ class Agent(object):
                     highest_val = self.memory[self.state][a]
                     key_of = a
             if highest_val == 0:
-                key_of = rnd.randrange(1, self.state + 1)
+                key_of = rnd.choice(choose_from)
             print("choosing bet {} with val {}".format(key_of, highest_val)) 
             return key_of
 
     def gamble_step(self):
         action = self.choose_action()
-        print("betting{}".format(action))
+        #print("betting{}".format(action))
         prior_state = self.state
         self.state += self.env.get_outcome(action)
         return prior_state,action
@@ -61,13 +69,29 @@ class Agent(object):
         while self.state >= 1 and self.state<100:
             last_act_and_last_state = self.gamble_step()
             actions_this_ep.append(last_act_and_last_state)
+        print(actions_this_ep)
         if self.state >= N:
             print("winner!")
             for i in range((len(actions_this_ep)-1),-1,-1):
-                self.memory[actions_this_ep[i][0]][actions_this_ep[i][1]] = (self.memory[actions_this_ep[i][0]][actions_this_ep[i][1]] + 1 * (D ** (len(actions_this_ep) - i)))
+                new_val = (self.memory[actions_this_ep[i][0]][actions_this_ep[i][1]] + (1 * (D ** (len(actions_this_ep)-i-1))))
+                #print(i)
+                #print(actions_this_ep[i])
+                print(D**(len(actions_this_ep)-i-1))
+                print("updating val for state {} act {} to {}".format(actions_this_ep[i][0],actions_this_ep[i][1], new_val))                
+                self.memory[actions_this_ep[i][0]][actions_this_ep[i][1]] = (self.memory[actions_this_ep[i][0]][actions_this_ep[i][1]] + (1 * (D ** (len(actions_this_ep)-i-1))))
         else:
-            print("1loser!!")
+            print("loser!!")
         self.state = X
+    
+    def print_state_bets(self):
+        state_bets_list = []
+        for i in self.memory:
+            highest_val = max(self.memory[i], key=self.memory[i].get)
+            val_tself=self.memory[i][highest_val]
+            tup = (i, highest_val,val_tself)
+            state_bets_list.append(tup)
+        for i in state_bets_list:
+            print("state {}".format(i))
 
 
 class Environment(object):
@@ -86,7 +110,9 @@ class Environment(object):
 if __name__ == '__main__':
     rnd.seed(42)
     env = Environment()
-    print(env.agent.memory)
-    for i in range(1000):
+    #print(env.agent.memory)
+    for i in range(50000):
         env.agent.gamble_episode()
+    env.agent.print_state_bets()
+    
     
